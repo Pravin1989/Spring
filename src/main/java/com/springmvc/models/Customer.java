236b72e39f6dd564b85840f2713f57f4ec441ec7
Pravin Naganath Budage <pravin.budage@Yash.com>
2017-08-24 15:52:29 +0530

    src

---
+++ src/main/java/com/springmvc/models/Customer.java
@@ -0,0 +1,49 @@
+package com.springmvc.models;
+
+public class Customer {
+	private String name;
+	private String address;
+	private float salary;
+	private String email;
+	private int customer_id;
+	
+	public int getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
